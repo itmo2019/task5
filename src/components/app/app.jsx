ca9013c5844563af4623dd55555679786b8385f2
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2019-04-06 19:27:20 +0300

    started

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './app.css';
 import Header from '../header/header';
+import Content from '../content/content';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
+        <Content/>
       </div>
     );
   }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,15 +9,15 @@ export class Header extends Component {
   render() {
     return (
       <header className="Header">
-        <img className="mail-header__tools-icon" src={logoLines} alt="logo-lines" />
+        <img className="Header__tools-icon" src={logoLines} alt="logo-lines" />
         <img
-          className="mail-header__logo"
+          className="Header__logo"
           src={yandexMailLogo}
           width="153"
           height="31"
           alt="yandex mail logo"
         />
-        <input className="main-header__search" type="search" placeholder="Поиск" />
+        <input className="Header__search" type="search" placeholder="Поиск" />
       </header>
     );
   }
