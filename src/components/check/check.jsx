e707b04db9388beb656605b3a45a2d03cf2fcb19
Yaroslav <mx.yarik@gmail.com>
2019-04-09 19:59:42 +0300

    merge LetterList with LetterBox change checkBox logic

--- src/components/check/check.jsx
+++ src/components/check/check.jsx
@@ -1,59 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './check.css';
 
-export default class Check extends Component {
-  static displayName = 'Check';
+export default function Check(props) {
+  const { isChecked, isMainCheckBox, handleCheckAllClick, handleMailCheckClick } = props;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isChecked: false
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.isCheckAll !== this.props.isCheckAll) {
-      this.setState({ isChecked: nextProps.isCheckAll });
-      return;
-    }
-    if (nextProps.hiddenMail !== this.props.hiddenMail) {
-      this.setState({ isChecked: false });
-      return;
-    }
-    if (nextProps.isMailChecked !== this.props.isMailChecked) {
-      this.setState({ isChecked: nextProps.isMailChecked });
-    }
-  }
-
-  toggleChange = () => {
-    this.setState(state => {
-      if (this.props.isMainCheckBox) {
-        this.props.handleCheckAllClick(!state.isChecked);
-        this.props.setCheckAll(!state.isChecked);
-      } else {
-        if (state.isChecked) {
-          this.props.setCheckAll(!state.isChecked);
-        }
-        this.props.handleMailCheckClick(!state.isChecked);
-      }
-      return { isChecked: !state.isChecked };
-    });
-  };
-
-  render() {
-    const { isChecked } = this.state;
-    return (
-      <div className="check">
-        <label>
-          <input
-            className="check__input"
-            type="checkbox"
-            checked={isChecked}
-            onChange={this.toggleChange}
-          />
-          <span className={`check__box ${isChecked ? 'check__box_checked' : ''}`} />
-        </label>
-      </div>
-    );
-  }
+  return (
+    <div className="check">
+      <label>
+        <input
+          className="check__input"
+          type="checkbox"
+          checked={isChecked}
+          onChange={isMainCheckBox ? handleCheckAllClick : handleMailCheckClick}
+        />
+        <span className={`check__box ${props.isChecked ? 'check__box_checked' : ''}`} />
+      </label>
+    </div>
+  );
 }
--- src/components/letterBox/letterBox.jsx
+++ src/components/letterBox/letterBox.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Toolbar from '../toolbar/toolbar';
 import LetterDialog from '../letterDialog/letterDialog';
-import LetterList from '../letterList/letterList';
 import SupportLine from '../supportLine/supportLine';
+import LetterGenerator from '../../utils/letterGenerator';
+import Letter from '../letter/letter';
 import './letterBox.css';
 
 function Hr() {
@@ -12,79 +13,184 @@ function Hr() {
 export default class LetterBox extends Component {
   static displayName = 'LetterBox';
 
+  MIN_TIMER_ADD_MAIL = 300000;
+
+  MAX_TIMER_ADD_MAIL = 600001;
+
+  MAX_MAIL_LIST_SIZE = 10;
+
+  REMOVE_LETTER_TIME = 500;
+
+  DELTA_TIME = 20;
+
+  letterId = 0;
+
+  letterGenerator = new LetterGenerator();
+
   constructor(props) {
     super(props);
     this.state = {
+      curPage: 1,
+      letters: [],
       isMailVisible: false,
       mailContent: undefined,
-      isCheckAll: false,
-      handleAddMailButtonClick: undefined,
-      handleRemoveButtonClick: undefined,
-      handleUnmarkButtonClick: undefined,
-      handleCheckAllClick: undefined
+      isCheckAll: false
     };
   }
-
-  handleMailExitClick = () => {
-    this.setState({ isMailVisible: false });
-  };
-
-  handleLetterDialog = mailContent => {
-    this.setState({ isMailVisible: true, mailContent });
-  };
 
   setCheckAll = value => {
     this.setState({ isCheckAll: value });
   };
 
-  updateHandleAddMailButtonClick = value => {
-    this.setState({ handleAddMailButtonClick: value });
+  getRandomLetter = () => {
+    const authorName = this.letterGenerator.getAuthorName();
+    return {
+      id: this.letterId++,
+      authorName,
+      authorLogo: this.letterGenerator.getAuthorLogo(authorName),
+      topic: this.letterGenerator.getTopic(),
+      body: this.letterGenerator.getLetterBody(),
+      date: this.letterGenerator.getDate(),
+      isUnread: true,
+      isChecked: false
+    };
   };
 
-  updateHandleRemoveButtonClick = value => {
-    this.setState({ handleRemoveButtonClick: value });
+  newMail = () => {
+    this.setState(state => {
+      const letters = state.letters.slice();
+      if (letters.length >= state.curPage * this.MAX_MAIL_LIST_SIZE) {
+        letters[state.curPage * this.MAX_MAIL_LIST_SIZE - 1].isChecked = false;
+      }
+      return {
+        letters: [this.getRandomLetter(), ...letters]
+      };
+    });
+    this.setCheckAll(false);
   };
 
-  updateHandleUnmarkButtonClick = value => {
-    this.setState({ handleUnmarkButtonClick: value });
+  doActionWithLetters = action => {
+    const { curPage, letters } = this.state;
+    const first = (curPage - 1) * this.MAX_MAIL_LIST_SIZE;
+    for (let i = Math.min(letters.length, curPage * this.MAX_MAIL_LIST_SIZE) - 1; i >= first; i--) {
+      if (letters[i].isChecked) {
+        action(i);
+      }
+    }
   };
 
-  updateHandleCheckAllClick = value => {
-    this.setState({ handleCheckAllClick: value });
+  removeAnimateLetter = index => {
+    setTimeout(() => {
+      this.setState(state => {
+        const letters = state.letters.slice();
+        letters[index].hasRemoveAnimation = true;
+        return { letters };
+      });
+      setTimeout(() => {
+        this.setState(state => {
+          const letters = state.letters.slice();
+          letters.splice(index, 1);
+          return { letters };
+        });
+      }, this.REMOVE_LETTER_TIME);
+    }, this.DELTA_TIME);
+  };
+
+  handleRemoveButtonClick = () => {
+    setTimeout(() => {
+      this.setCheckAll(false);
+    }, (this.REMOVE_LETTER_TIME * 2) / 3);
+    this.doActionWithLetters(this.removeAnimateLetter);
+  };
+
+  unmarkLetter = index => {
+    this.setState(state => {
+      const letters = state.letters.slice();
+      letters[index].isUnread = false;
+      return { letters };
+    });
+  };
+
+  handleUnmarkButtonClick = () => {
+    this.doActionWithLetters(this.unmarkLetter);
+  };
+
+  handleMailCheckClick = index => {
+    this.setState(state => {
+      const letters = state.letters.slice();
+      const checked = letters[index].isChecked;
+      const isCheckAllNew = checked ? !checked : state.isCheckAll;
+      letters[index].isChecked = !checked;
+      return { letters, isCheckAll: isCheckAllNew };
+    });
+  };
+
+  handleCheckAllClick = () => {
+    this.setState(state => {
+      const letters = state.letters.slice();
+      const size = Math.min(letters.length, state.curPage * this.MAX_MAIL_LIST_SIZE);
+      for (let i = (state.curPage - 1) * this.MAX_MAIL_LIST_SIZE; i < size; i++) {
+        letters[i].isChecked = !state.isCheckAll;
+      }
+      return { letters, isCheckAll: !state.isCheckAll };
+    });
+  };
+
+  handleMailClick = index => {
+    this.setState(state => {
+      const mailContent = (
+        <div>
+          <div className="letter__topic_is-open">{state.letters[index].topic}</div>
+          {state.letters[index].body}
+        </div>
+      );
+      return { isMailVisible: true, mailContent };
+    });
+    this.unmarkLetter(index);
+  };
+
+  handleMailExitClick = () => {
+    this.setState({ isMailVisible: false });
   };
 
   render() {
-    const {
-      isMailVisible,
-      mailContent,
-      isCheckAll,
-      handleAddMailButtonClick,
-      handleRemoveButtonClick,
-      handleUnmarkButtonClick,
-      handleCheckAllClick
-    } = this.state;
+    const { curPage, letters, isMailVisible, mailContent, isCheckAll } = this.state;
+
+    const listLetters = letters.map((letter, index) => (
+      <Letter
+        key={letter.id}
+        className="letter-box__letter"
+        authorName={letter.authorName}
+        authorLogo={letter.authorLogo}
+        topic={letter.topic}
+        body={letter.body}
+        date={letter.date}
+        hiddenMail={
+          letters.length > curPage * this.MAX_MAIL_LIST_SIZE &&
+          index >= curPage * this.MAX_MAIL_LIST_SIZE
+        }
+        hasRemoveAnimation={letter.hasRemoveAnimation}
+        handleMailCheckClick={this.handleMailCheckClick.bind(this, index)}
+        handleMailClick={this.handleMailClick.bind(this, index)}
+        isUnread={letter.isUnread}
+        isChecked={letter.isChecked}
+      />
+    ));
+
     return (
       <div className={`letter-box ${this.props.className}`}>
         <Toolbar
-          isCheckAll={isCheckAll}
-          setCheckAll={this.setCheckAll}
-          handleAddMailButtonClick={handleAddMailButtonClick}
-          handleRemoveButtonClick={handleRemoveButtonClick}
-          handleUnmarkButtonClick={handleUnmarkButtonClick}
-          handleCheckAllClick={handleCheckAllClick}
+          isChecked={isCheckAll}
+          handleAddMailButtonClick={this.newMail}
+          handleRemoveButtonClick={this.handleRemoveButtonClick}
+          handleUnmarkButtonClick={this.handleUnmarkButtonClick}
+          handleCheckAllClick={this.handleCheckAllClick}
         />
         <Hr />
         <LetterDialog isMailVisible={isMailVisible} handleMailExitClick={this.handleMailExitClick}>
           {mailContent}
         </LetterDialog>
-        <LetterList
-          updateHandleAddMailButtonClick={this.updateHandleAddMailButtonClick}
-          updateHandleRemoveButtonClick={this.updateHandleRemoveButtonClick}
-          updateHandleUnmarkButtonClick={this.updateHandleUnmarkButtonClick}
-          updateHandleCheckAllClick={this.updateHandleCheckAllClick}
-          handleLetterDialog={this.handleLetterDialog}
-          setCheckAll={this.setCheckAll}
-        />
+        <ul className="letter-box__letter-list">{listLetters}</ul>
         <div className="letter-box__support-line">
           <Hr />
           <SupportLine />
