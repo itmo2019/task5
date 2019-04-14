66005b85e1cc29abaae1359dd369cd03cb263f6b
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-04-14 14:10:06 +0300

    Introduce template of main page content

---
+++ src/app/page-content/page-content.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { MailToolbar } from '../mail-toolbar/mail-toolbar';
+
+import './page-content.css';
+import './__content/page-content__content.css';
+import './__left-toolbar/page-content__left-toolbar.css';
+
+export class PageContent extends Component {
+  render() {
+    return (
+      <div className="page-content">
+        <article className="page-content__left-toolbar">
+          <MailToolbar />
+        </article>
+        <article className="page-content__content" />
+      </div>
+    );
+  }
+}
