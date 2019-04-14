a545392ec9db6a747f96de80ebe685ab20b0a83f
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-04-14 14:50:35 +0300

    Introduce Inbox footer

---
+++ src/app/inbox/__footer/inbox-footer.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './inbox__footer.css';
+import './inbox__footer-date.css';
+import './inbox__footer-element.css';
+
+export class InboxFooter extends Component {
+  render() {
+    return (
+      <footer className="inbox__footer">
+        <a className="inbox__footer-element" href="https://yandex.ru/support/mail/">
+          Помощь и обратная связь
+        </a>
+        <a className="inbox__footer-element" href="https://yandex.ru/adv/">
+          Реклама
+        </a>
+        <span className="inbox__footer-date">© 2001—2018,</span>
+        <a className="inbox__footer-element" href="https://yandex.ru/">
+          Яндекc
+        </a>
+      </footer>
+    );
+  }
+}
--- src/app/inbox/inbox.jsx
+++ src/app/inbox/inbox.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './inbox.css';
 import { InboxHeader } from './__header/inbox-header';
+import { InboxFooter } from './__footer/inbox-footer';
 
 export class Inbox extends Component {
   render() {
     return (
       <div className="inbox">
         <InboxHeader />
+        <InboxFooter />
       </div>
     );
   }
