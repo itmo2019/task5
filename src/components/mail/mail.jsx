5b33188ea43580b90dd3ce421772e678df811b14
Good-Morning <guuud.morning@gmail.com>
2019-04-14 23:34:14 +0300

    -interblock-interaction
    '.block0__elm + .block1' seems to have interblock interaction.
    Refactored.

--- src/components/mail/mail.jsx
+++ src/components/mail/mail.jsx
@@ -8,8 +8,12 @@ import './mail.css';
 function Mail(props /* {mailID, callbacks, sender, avatar, title, date, article, classList, checked} */) {
     return  <div className="mail" key={props.mailID}>
                 <input className="mail__checkbox" id={props.mailID} type="checkbox" />
-                <MailTitle {...props} />
-                <MailArticle mailID={props.mailID} body={props.article} />
+                <div className="mail__mail-title-wrapper">
+                    <MailTitle {...props} />
+                </div>
+                <div className="mail__mail-article-wrapper">
+                    <MailArticle mailID={props.mailID} body={props.article} />
+                </div>
             </div>
 }
 
