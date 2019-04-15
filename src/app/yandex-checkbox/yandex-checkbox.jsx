fc217671dc6f86c87554520ef12460982c48458c
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-04-15 20:21:02 +0300

    Enable checkbox checking

--- src/app/yandex-checkbox/yandex-checkbox.jsx
+++ src/app/yandex-checkbox/yandex-checkbox.jsx
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import './yandex-checkbox.css';
 
 export class YandexCheckbox extends Component {
-  checkboxAction;
+  constructor(props) {
+    super(props);
+    this.id = props.id;
+    this.state = { isChecked: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  checkboxId;
-
-  isChecked;
+  handleChange() {
+    this.setState(state => ({
+      isChecked: !state.isChecked
+    }));
+  }
 
   render() {
     return (
       <div className="yandex-checkbox__field">
-        <label htmlFor={`checkbox_${this.props.checkboxId}`}>
+        <label htmlFor={`checkbox_${this.props.id}`}>
           <input
             className="yandex-checkbox__input"
             type="checkbox"
-            id={`checkbox_${this.props.checkboxId}`}
-            checked={this.props.isChecked}
-            onChange={this.props.checkboxAction}
+            id={`checkbox_${this.props.id}`}
+            onChange={this.handleChange}
           />
           <span
-            className={`yandex-checkbox ${this.props.isChecked ? 'yandex-checkbox-checked' : ''}`}
+            className={`yandex-checkbox ${this.state.isChecked ? 'yandex-checkbox-checked' : ''}`}
           />
         </label>
       </div>
