3524502c4c55071fcfd2c3b3903d48fc94b1cdbe
BirdyDeveloper <Alex.PKZDL@yandex.by>
2019-05-06 22:24:42 +0300

    test: provider added

--- src/app/app.test.jsx
+++ src/app/app.test.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
 
 import { App } from './app';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
